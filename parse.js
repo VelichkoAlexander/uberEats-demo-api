c74b6afb340fa9b5eb309b391d492b8f5b5f3588
Alexandr Velichko <alvi@mail.ua>
2020-02-16 10:48:47 +0200

    added parse module

--- parse.js
+++ parse.js
@@ -1,23 +1,52 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
-const debug = require("debug")("uberEats");
+const fs = require("fs");
 
 const url = `https://mariupol.mixfood.ua/`;
+const api = [];
 
 const parse = async () => {
   const response = await request(url);
 
-  let $ = cheerio.load(response);
+  let $ = cheerio.load(response, {
+    xml: {
+      normalizeWhitespace: true
+    }
+  });
 
   const cafeCard = $(".cafe-card");
-  cafeCard.each((i, card) => {
-    console.log(
-      $(card)
-        .find(".cafe-card__title")
+  cafeCard.each((id, card) => {
+    let domCard = $(card);
+    api.push({
+      id,
+      title: domCard.find(".cafe-card__title").text(),
+      type: domCard.find(".cafe-card__subtitle").text(),
+      img: domCard
+        .find(".cafe-card__image")
+        .css("background-image")
+        .replace(/.*\s?url\([\'\"]?/, "")
+        .replace(/[\'\"]?\).*/, ""),
+      price: "₴₴",
+      rating: domCard
+        .find(".star-rating")
+        .attr("class")
+        .replace(/\D/g, ""),
+      reviews: domCard
+        .find(".blue-lined-link")
         .text()
-    );
+        .replace(/\D/g, "")
+    });
+
+    storeData(api, "./data/output.json");
   });
-  // console.log(container);
 };
 
 parse();
+
+const storeData = (data, path) => {
+  try {
+    fs.writeFileSync(path, JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
